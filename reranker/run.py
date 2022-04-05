2c0c4378b56220b285d2ccb3e5dc20fc64e06188
csacro <50425802+csacro@users.noreply.github.com>
2022-04-05 10:41:25 +0200

    allow to use Longformer model and to load pretrained model from given path

--- reranker/run.py
+++ reranker/run.py
@@ -9,7 +9,9 @@ from triplet_loss import TripletLoss
 
 
 def train_and_evaluate_SciBERT_Reranker(train_data, documents_per_query, val_data=None, test_data=None,
-                                        exp_name: str = 'unknown_experiment', use_cased=False):
+                                        exp_name: str = 'unknown_experiment',
+                                        use_cased=False, use_longformer=False,
+                                        load_model=None):
     """
     fine-tunes SciBERT for Local Citation Recommendation and evaluates resulting model
     Args:
@@ -18,7 +20,9 @@ def train_and_evaluate_SciBERT_Reranker(train_data, documents_per_query, val_dat
         test_data: test data with tsv file columns 'Query', 'Document', 'Relevant'
         documents_per_query: tells how many documents per query are in the dataframes
         exp_name: name of the experiment to be included in the output directory
-        use_cased: whether to use the cased version of SciBERT instead of the uncased
+        use_cased: whether to use cased or uncased input with the respective model if available
+        use_longformer: use the Longformer model instead of the SciBERT model
+        load_model: path to the model to be loaded as a starting point
     """
 
     # config options
@@ -36,7 +40,7 @@ def train_and_evaluate_SciBERT_Reranker(train_data, documents_per_query, val_dat
         # "reprocess_input_data": True,
         # "use_cached_eval_features": False,
 
-        "num_train_epochs": 5,  # todo figure out value (we evaluate at every epoch during training)
+        "num_train_epochs": 5,
         "train_batch_size": 32,  # according to paper
         "gradient_accumulation_steps": 2,  # according to paper
         "learning_rate": 1e-5,  # according to paper
@@ -53,7 +57,7 @@ def train_and_evaluate_SciBERT_Reranker(train_data, documents_per_query, val_dat
         # "evaluate_during_training_verbose": False,
         # "evaluate_each_epoch": True,
 
-        "do_lower_case": not use_cased,  # set according to used model
+        "do_lower_case": not use_cased,
         "max_seq_length": 512,  # maximum length
         "manual_seed": 0,  # set some value
 
@@ -71,9 +75,18 @@ def train_and_evaluate_SciBERT_Reranker(train_data, documents_per_query, val_dat
     }
 
     # create SciBERT classifier
-    model = ClassificationModel('bert',
-                                'allenai/scibert_scivocab_cased' if use_cased else 'allenai/scibert_scivocab_uncased',
-                                args=reranker_args, loss_fct=TripletLoss(m=0.1))
+    if load_model:
+        model_name = 'longformer' if use_longformer else 'bert'
+        model = ClassificationModel(model_name, load_model, args=reranker_args, loss_fct=TripletLoss(m=0.1))
+    else:
+        if use_longformer:
+            model = ClassificationModel('longformer',
+                                        'allenai/longformer-base-4096',
+                                        args=reranker_args, loss_fct=TripletLoss(m=0.1))
+        else:
+            model = ClassificationModel('bert',
+                                        'allenai/scibert_scivocab_cased' if use_cased else 'allenai/scibert_scivocab_uncased',
+                                        args=reranker_args, loss_fct=TripletLoss(m=0.1))
 
     # train model
     model.train_model(train_data, eval_df=val_data,
@@ -96,19 +109,26 @@ def train_and_evaluate_SciBERT_Reranker(train_data, documents_per_query, val_dat
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--documents_per_query", help="Amount of documents per query in the data.", required=True,
-                        type=int)
+    parser.add_argument("--documents_per_query", type=int, required=True,
+                        help="Amount of documents per query in the data.")
     parser.add_argument("--train_data", help="Location of the created training data tsv file.", required=True, type=str)
-    parser.add_argument("--validation_data", help="Location of the created validation data tsv file.", default=None,
-                        type=str)
-    parser.add_argument("--test_data", help="Location of the created test data tsv file.", default=None, type=str)
-    parser.add_argument("--exp_name", help="Name of the experiment (used in output directory).",
-                        default='unknown_experiment', type=str)
-    parser.add_argument("--use_cased", help="Whether to use the cased model variant.", action='store_true',
-                        default=False)
+    parser.add_argument("--validation_data", type=str, default=None,
+                        help="Location of the created validation data tsv file.")
+    parser.add_argument("--test_data", default=None, type=str,
+                        help="Location of the created test data tsv file.")
+    parser.add_argument("--exp_name", type=str, default='unknown_experiment',
+                        help="Name of the experiment (used in output directory).")
+    parser.add_argument("--use_cased", action='store_true', default=False,
+                        help="Whether to use cased input with the respective model variant (if available).")
+    parser.add_argument("--use_longformer", action='store_true', default=False,
+                        help='Whether to switch from a SciBERT to a Longformer model'
+                             '(also needs to be set properly in case of load_model usage).')
+    parser.add_argument("--load_model", type=str, default=None,
+                        help='Path to the model that should be loaded as a starting point.')
     args = parser.parse_args()
 
     # train and evaluate model
     train_and_evaluate_SciBERT_Reranker(args.train_data, val_data=args.validation_data, test_data=args.test_data,
-                                        documents_per_query=args.documents_per_query,
-                                        use_cased=args.use_cased, exp_name=args.exp_name)
+                                        documents_per_query=args.documents_per_query, exp_name=args.exp_name,
+                                        use_cased=args.use_cased, use_longformer=args.use_longformer,
+                                        load_model=args.load_model)
