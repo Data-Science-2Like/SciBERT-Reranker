83f0757abae83d8dee6009e25baf0a7744048dc7
csacro <50425802+csacro@users.noreply.github.com>
2022-09-07 09:31:26 +0200

    moved experiment scripts to respective folders

--- baseline/baseline_prefetcher+reranker.sh
+++ baseline/baseline_prefetcher+reranker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ..
 
 python baseline/run.py --paper data_s2orc/papers.jsonl --contexts data_s2orc/test_contexts.jsonl --candidates_per_context dataset/s2orc_prefetchedfile_BaselineCountbased_20_top_candidates_per_query.joblib --citation_context_fields citation_context title paragraph --no_mask --mrr --recall_k 10 --exp_name baseline-Prefetcher-BaselineCountbased20
 python baseline/run.py --paper data_s2orc/papers.jsonl --contexts data_s2orc/test_contexts.jsonl --candidates_per_context dataset/s2orc_prefetchedfile_BaselineCountbased_50_top_candidates_per_query.joblib --citation_context_fields citation_context title paragraph --no_mask --mrr --recall_k 10 --exp_name baseline-Prefetcher-BaselineCountbased50
